ff2b2c3ca3d905bd76e9122cbcd5935af71e433a
Haotian Yuan <yuanhaotianv@gmail.com>
2026-08-09 17:37:38 +0800

    Bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bowdet",
-    version="0.2.1",
+    version="0.2.2",
     description="Audio-based bow-change and note-boundary detection for string instruments",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
